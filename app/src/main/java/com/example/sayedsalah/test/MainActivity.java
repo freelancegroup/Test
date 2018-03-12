91d6d494c25f52d63db7700d2ae8a8538479a622
freelance team <mohamedmzd0@gmail.com>
2018-03-13 00:55:37 +0200

    this is test commit

--- app/src/main/java/com/example/sayedsalah/test/MainActivity.java
+++ app/src/main/java/com/example/sayedsalah/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sayedsalah.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //start
+        Toast.makeText(this, "12345", Toast.LENGTH_SHORT).show();
     }
 }
