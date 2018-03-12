0268aa700a30b8755eaf968848b06d50820f1970
freelancegroup <37305812+freelancegroup@users.noreply.github.com>
2018-03-13 01:03:36 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/sayedsalah/test/MainActivity.java
+++ app/src/main/java/com/example/sayedsalah/test/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //start
+        //start2
         Toast.makeText(this, "12345", Toast.LENGTH_SHORT).show();
     }
 }
